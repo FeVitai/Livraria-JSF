36a83cc20f3ffba9732ce2b939de324de7e042ef
FeVitai <felipevitai93@gmail.com>
2020-04-16 15:50:05 -0300

    Atualizacao do JSF da versão 2.1.14 para 2.2.13

--- src/br/com/caelum/livraria/bean/AutorBean.java
+++ src/br/com/caelum/livraria/bean/AutorBean.java
@@ -1,5 +1,8 @@
 package br.com.caelum.livraria.bean;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -8,21 +11,47 @@ import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.util.ForwardView;
 @ViewScoped
 @ManagedBean
-public class AutorBean {
+public class AutorBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private Autor autor = new Autor();
 
 	public Autor getAutor() {
 		return autor;
 	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
 
 	public ForwardView gravar() {
 		System.out.println("Gravando autor " + this.autor.getNome());
-
-		new DAO<Autor>(Autor.class).adiciona(this.autor);
-
+		
+		if(this.autor.getNome() == null) {
+			new DAO<Autor>(Autor.class).adiciona(this.autor);
+		} else {
+			new DAO<Autor>(Autor.class).atualiza(this.autor);
+		}
+		
 		this.autor = new Autor();
 		
 		return new ForwardView("livro");
 	}
+	
+	public void remover(Autor autor) {
+		System.out.println("Removendo autor");
+		new DAO<Autor>(Autor.class).remove(autor);
+	}
+	
+	public void carregar(Autor autor) {
+		System.out.println("Carregando autor");
+		this.autor = autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+	
+	
 }
