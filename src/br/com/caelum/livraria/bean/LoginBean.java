234b3074a9bf87ccdc47c8dc7cd9ed7de067778e
FeVitai <felipevitai93@gmail.com>
2020-04-19 19:29:13 -0300

    Melhorias das funções de autenticação

--- src/br/com/caelum/livraria/bean/LoginBean.java
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -1,7 +1,9 @@
 package br.com.caelum.livraria.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
@@ -15,13 +17,29 @@ public class LoginBean {
 	public String efetuaLogin() {
 		System.out.println("Fazendo login do usuário " + this.usuario.getEmail());
 		
+		FacesContext context = FacesContext.getCurrentInstance();
+		
 		boolean existe = new UsuarioDao().existe(this.usuario);
 		
 		if(existe) {
+			
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
+			
 			return "livro?faces-redirect=true";
 		}
+		
+		context.getExternalContext().getFlash().setKeepMessages(true);
+		context.addMessage(null, new FacesMessage("Usuário não encontrado"));
 			
-		return null;
+		return "login?faces-redirect=true";
+	}
+	
+	public String deslogar() {
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap().remove("usuarioLogado");
+		
+		return "login?faces-redirect=true";
 	}
 
 	//GETTERS AND SETTERS
